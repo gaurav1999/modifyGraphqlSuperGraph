0e08812cd9cdb95815f8bb6d9ec3b4a9f754ec5d
Gaurav <gaurav@DBMCIs-MacBook-Air.local>
2024-03-11 12:29:34 +0530

    fix: support regex and bug fix for description

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -8,14 +8,45 @@ import { SupergraphSdlHookOptions } from '@apollo/gateway/dist/config';
 import { parseGraphQLSDL } from '@graphql-tools/utils';
 import { print, Kind, DefinitionNode, SchemaDefinitionNode } from 'graphql';
 
+import 'dotenv/config';
+
+export enum GATEWAY_VISIBILITY_CONTROL  {
+  DESCRIPTION = "DESCRIPTION",
+  DISABLED = "DISABLED",
+  REGEX = "REGEX"
+}
 
 
 class CustomIntrospectClass extends IntrospectAndCompose {
 
+  private validateRegex(regexString: string): string {
+    try {
+      new RegExp(regexString);
+      return regexString; // Regex is valid
+    } catch (error) {
+      return null; // Regex is invalid
+    }
+  }
+
+  private validateResolverByRegex(resolver: string) {
+    const includes = this.validateRegex(process.env.INTERNAL_GRAPH_REGEX_INCLUDE);
+    const excludes = this.validateRegex(process.env.INTERNAL_GRAPH_REGEX_EXCLUDE);
+    //If include regex is provided then we only allow the matches which match include.
+    if(!!includes && resolver.match(includes)) return true;
+    if(!!excludes) {
+      //Exclude means that we don't want the resolvers matching this pattern to be exposed.
+     if(resolver.match(excludes)) return false;
+     //We want to focus on when to allow the resolver
+     //We will only allow this when the resolver is valid either it's included and matched or either it's not excluded
+     if(!includes) return true;
+    }
+    return false;
+  }
+
   private validateExposureAndSanitizeDescription(description: string) {
     const data = { expose: false, field: description };
     if(!description) return data;
-    const gatewayName = process.env.GATEWAY_NAME || 'default';
+    const gatewayName = process.env.INTERNAL_GRAPH_GATEWAY_NAME || "default";
     const pattern = /@gateway\((.*?)\)/;
     const match = description.match(pattern);
     if(!match) return data;
@@ -34,81 +65,90 @@ class CustomIntrospectClass extends IntrospectAndCompose {
   }
 
 
+
   async initialize({ update, getDataSource, healthCheck }: SupergraphSdlHookOptions): Promise<{ supergraphSdl: string; cleanup: () => Promise<void>; }> {
     const { supergraphSdl, cleanup } = await super.initialize({ update, getDataSource, healthCheck });
-    const schema = parseGraphQLSDL(null,supergraphSdl, { });
-    
+    const schema = parseGraphQLSDL(null, supergraphSdl, {});
+
     const filteredDefinitions: DefinitionNode[] = [];
     const schemaNodeDef = schema.document.definitions.find(item => item.kind === Kind.SCHEMA_DEFINITION) as SchemaDefinitionNode;
-    let resolverMap = { };
+    let resolverMap = {};
 
-    if(schemaNodeDef) {
+    if (schemaNodeDef) {
       const queryOp = schemaNodeDef.operationTypes.find(item => item.operation === 'query');
-       const mutationOp = schemaNodeDef.operationTypes.find(item => item.operation === 'mutation');
-       resolverMap['Mutation'] = mutationOp;
-       resolverMap['Query'] = queryOp;
+      const mutationOp = schemaNodeDef.operationTypes.find(item => item.operation === 'mutation');
+      resolverMap['Mutation'] = mutationOp;
+      resolverMap['Query'] = queryOp;
     }
 
-    for(let index = 0; index < schema.document.definitions.length; index++) {
+    const gatewayControl = process.env.GATEWAY_CONTROL || GATEWAY_VISIBILITY_CONTROL.DISABLED;
 
+    for (let index = 0; index < schema.document.definitions.length; index++) {
 
       const node = schema.document.definitions[index];
 
-      if(node.kind === Kind.SCHEMA_DEFINITION) continue;
+      if (node.kind === Kind.SCHEMA_DEFINITION) continue;
 
-      if(node.kind === Kind.OBJECT_TYPE_DEFINITION) {
-        if(node.name.value === 'Query' || node.name.value === 'Mutation') {
-          const fields = [];
-          node.fields.forEach(item => {
-            const { expose, field } = this.validateExposureAndSanitizeDescription(item?.description?.value);
-            if(expose) {
-              fields.push({
-                ...item,
-                description: { kind: Kind.STRING, value: field }
-              })
+      if (gatewayControl === GATEWAY_VISIBILITY_CONTROL.DISABLED) { filteredDefinitions.push(node); continue; };
+
+      if (node.kind === Kind.OBJECT_TYPE_DEFINITION) {
+        if (node.name.value === 'Query' || node.name.value === 'Mutation') {
+          if (gatewayControl !== GATEWAY_VISIBILITY_CONTROL.DISABLED) {
+            const fields = [];
+            node.fields.forEach(item => {
+              if(gatewayControl === GATEWAY_VISIBILITY_CONTROL.DESCRIPTION) {
+                //Only process nodes with valid description
+                if(item.description) {
+                  const { expose, field } = this.validateExposureAndSanitizeDescription(item?.description?.value);
+                  if (expose) {
+                    fields.push({
+                      ...item,
+                      description: { kind: Kind.STRING, value: field }
+                    })
+                  }
+                }
+                //If any Query/Mutation Node is not having description it would be filtered out and removed.
+              } else {
+                //Validating Regex
+                if(this.validateResolverByRegex(item.name.value)) {
+                  fields.push(item);
+                }
+              }
+
+            })
+
+            if (!fields.length) {
+              //If we don't have any mutation or query we need to remove from schema.
+              delete resolverMap[node.name.value];
+              continue;
             }
-          })
-        if(!fields.length) {
-          //If we don't have any mutation or query we need to remove from schema.
-          delete resolverMap[node.name.value];
-          continue;
-        }
-          const updateNode = { ...node, fields };
-          filteredDefinitions.push(updateNode);
-          continue;
+
+            const updateNode = { ...node, fields };
+            filteredDefinitions.push(updateNode);
+            continue;
+
+          }
+
+
+
+
         }
       }
-
       //Pushing other type of nodes irrespective of filters
       filteredDefinitions.push(node);
     }
-
 
     filteredDefinitions.push({
       ...schemaNodeDef,
       operationTypes: Object.values(resolverMap)
     })
 
-    
-    return { 
-      supergraphSdl: print({ kind: Kind.DOCUMENT, definitions: filteredDefinitions }), 
-      cleanup
-    } ;
-    
-    
-    const modifiedSchemaString = supergraphSdl.split('\n')
-    .filter(line => !line.includes('@deprecated'))
-    .join('\n');
     return {
-      supergraphSdl,
+      supergraphSdl: print({ kind: Kind.DOCUMENT, definitions: filteredDefinitions }),
       cleanup
-    }
+    };
+
   }
-
-  // private async updateSupergraphSdl() {
-  //   return this.updateSupergraphSdl();
-  // }
-
 
 }
 
